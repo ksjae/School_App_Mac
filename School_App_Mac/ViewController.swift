79a31bce0317b9c5becd1a29672d49fd67c42da9
LazyD_MainDev <webmaster@ksjit.com>
2019-01-13 16:09:45 +0900

    Initial Commit

---
+++ School_App_Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  School_App_Mac
+//
+//  Created by 김승재 on 13/01/2019.
+//  Copyright © 2019 Seungjae Kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ School_App_MacTests/School_App_MacTests.swift
@@ -0,0 +1,34 @@
+//
+//  School_App_MacTests.swift
+//  School_App_MacTests
+//
+//  Created by 김승재 on 13/01/2019.
+//  Copyright © 2019 Seungjae Kim. All rights reserved.
+//
+
+import XCTest
+@testable import School_App_Mac
+
+class School_App_MacTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
